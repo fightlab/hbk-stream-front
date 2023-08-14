5e0d4e36a42ef47d39d97a83bac7b408167b0a25
Mahesh Makani <mahesh@makani.dev>
2023-08-14 19:23:45 +0100

    set up vsfxi top8

--- src/ui/Routes/DET8/index.tsx
+++ src/ui/Routes/DET8/index.tsx
@@ -27,13 +27,13 @@ const styles: Styles = {
   },
   root: theme.container,
   match: {
-    padding: "4px 0",
-  }
+    padding: "3px 0",
+  },
 };
 
 const { white } = theme;
 
-const smallText: IThemeFont = merge({}, theme.engschrift, {
+const smallText: IThemeFont = merge({}, theme.dinCondensedRegular, {
   fontSize: "24pt",
 });
 
@@ -53,44 +53,42 @@ const Match = ({
   hideScores = false,
   match,
   padding,
-}: MatchProps) => {
-  return (
-    <>
+}: MatchProps) => (
+  <>
+    <TextBox
+      top={top}
+      right={right}
+      textAlign="right"
+      maxWidth="235px"
+      width="235px"
+      backgroundColor={theme.transparent}
+      truncate
+    >
+      <Text position="relative" font={smallText} color={white}>
+        {match.player1DisplayName}
+      </Text>
+      <div className={padding} />
+      <Text position="relative" font={smallText} color={white}>
+        {match.player2DisplayName}
+      </Text>
+    </TextBox>
+    {!hideScores && (
       <TextBox
         top={top}
-        right={right}
-        textAlign="right"
-        maxWidth="235px"
-        width="235px"
+        right={right && right - 40}
         backgroundColor={theme.transparent}
-        truncate
       >
         <Text position="relative" font={smallText} color={white}>
-          {match.player1DisplayName}
+          {match.player1Score}
         </Text>
-        <div className={padding}></div>
+        <div className={padding} />
         <Text position="relative" font={smallText} color={white}>
-          {match.player2DisplayName}
+          {match.player2Score}
         </Text>
       </TextBox>
-      {!hideScores && (
-        <TextBox
-          top={top}
-          right={right && right - 52}
-          backgroundColor={theme.transparent}
-        >
-          <Text position="relative" font={smallText} color={white}>
-            {match.player1Score}
-          </Text>
-          <div className={padding}></div>
-          <Text position="relative" font={smallText} color={white}>
-            {match.player2Score}
-          </Text>
-        </TextBox>
-      )}
-    </>
-  );
-};
+    )}
+  </>
+);
 
 interface IDEWT8Props extends WithStylesProps<typeof styles> {}
 
@@ -124,14 +122,14 @@ class DET8 extends React.Component<IDEWT8Props, IDEWT8State> {
         venue: "BrewDog Brighton",
         showTimer: false,
         startText: "Starts",
-      }
+      },
     };
 
     this.io.on("matches", ({ bracket, matches }) => {
       console.log("received matches");
       this.setState({ matches, bracket });
     });
-    
+
     this.io.on("prestream", (prestream) => {
       this.setState({ info: prestream });
     });
@@ -143,10 +141,10 @@ class DET8 extends React.Component<IDEWT8Props, IDEWT8State> {
     setInterval(() => {
       const { bracket } = this.state;
       if (bracket) {
-        console.log('get bracket matches', bracket);
+        console.log("get bracket matches", bracket);
         this.io.emit("bracket-get-matches", bracket);
       }
-    }, 60 * 1000)
+    }, 60 * 1000);
   }
 
   render() {
@@ -155,43 +153,89 @@ class DET8 extends React.Component<IDEWT8Props, IDEWT8State> {
     return (
       <div className={classes.root}>
         {/* Losers Top 8 1 */}
-        <Match padding={classes.match} top={667} right={1553} match={matches[0]} />
+        <Match
+          padding={classes.match}
+          top={670}
+          right={1541}
+          match={matches[0]}
+        />
         {/* Losers Top 8 2 */}
-        <Match padding={classes.match} top={883} right={1553} match={matches[1]} />
+        <Match
+          padding={classes.match}
+          top={887}
+          right={1541}
+          match={matches[1]}
+        />
         {/* Losers QF 1 */}
-        <Match padding={classes.match} top={651} right={1185} match={matches[2]} />
+        <Match
+          padding={classes.match}
+          top={654}
+          right={1174}
+          match={matches[2]}
+        />
         {/* Losers QF 2 */}
-        <Match padding={classes.match} top={865} right={1185} match={matches[3]} />
+        <Match
+          padding={classes.match}
+          top={868}
+          right={1174}
+          match={matches[3]}
+        />
         {/* Losers Semi Final */}
-        <Match padding={classes.match} top={757} right={813} match={matches[4]} />
+        <Match
+          padding={classes.match}
+          top={760}
+          right={802}
+          match={matches[4]}
+        />
         {/* Losers Final */}
-        <Match padding={classes.match} top={741} right={453} match={matches[5]} />
+        <Match
+          padding={classes.match}
+          top={744}
+          right={442}
+          match={matches[5]}
+        />
         {/* Winners Semi Final 1 */}
-        <Match padding={classes.match} top={98} right={1553} match={matches[6]} />
+        <Match
+          padding={classes.match}
+          top={102}
+          right={1542}
+          match={matches[6]}
+        />
         {/* Winners Semi Final 2 */}
-        <Match padding={classes.match} top={444} right={1553} match={matches[7]} />
+        <Match
+          padding={classes.match}
+          top={447}
+          right={1541}
+          match={matches[7]}
+        />
         {/* Winners Final */}
-        <Match padding={classes.match} top={271} right={1185} match={matches[8]} />
+        <Match
+          padding={classes.match}
+          top={274}
+          right={1174}
+          match={matches[8]}
+        />
         {/* Grand Final */}
-        <Match padding={classes.match}
-          top={269}
-          right={660}
+        <Match
+          padding={classes.match}
+          top={273}
+          right={650}
           match={matches[9]}
           identifierShift={false}
         />
         {/* Game info */}
         <TextBox
           // event text
-          top={247}
-          left={1450}
-          textAlign="left"
+          top={253}
+          left={1455}
+          textAlign="middle"
           width="100%"
           padding={0}
           backgroundColor={theme.transparent}
         >
           <Text
-            font={merge({}, theme.engschrift, {
-              fontSize: "52px",
+            font={merge({}, theme.dinCondensedRegular, {
+              fontSize: "50px",
             })}
           >
             {info.event}
